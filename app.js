d607b5d2a35af83d40c6d0f569075852d7fd33e4
eduardsdn <eduard.sdn@gmail.com>
2022-09-29 17:00:31 +0200

    Computational logic is now present in operate(), yet it only computes two numbers as for now. Operators + and - are implemented at the moment. Two operants are displayed on the display step by step as well as computed result

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 
-
-
 let one = document.querySelector("#one")
 let two = document.querySelector("#two")
 let three = document.querySelector("#three")
@@ -11,68 +9,99 @@ let seven = document.querySelector("#seven")
 let eight = document.querySelector("#eight")
 let nine = document.querySelector("#nine")
 let zero = document.querySelector("#zero")
-
 let divide = document.querySelector("#divide")
 let multiply = document.querySelector("#multiply")
 let plus = document.querySelector("#plus")
 let minus = document.querySelector("#minus")
 let dot = document.querySelector("#dot")
 let equal = document.querySelector("#equal")
-
 let clear = document.querySelector("#clearBtn")
 let deleteBtn = document.querySelector("#deleteBtn")
-
 let display = document.querySelector(".display")
 
 
-let numbers = [one,two,three,four,five,six,seven,eight,nine,zero]
+let numbers = [one,two,three,four,five,six,seven,eight,nine,zero,dot]
+let operators = [plus,minus,divide,multiply,dot]
 
-//box.textContent = 'New text'
 
 let inputCollection = []
+let inputNumber 
+let operant 
+let computed
+
+
 
 numbers.forEach(item => {
-    item.addEventListener('click', event => {
-        
-        if (item == one){
-            inputCollection.push("1")
-        }
-        else if (item == two) {
-            inputCollection.push("2")
-        }
-        else if (item == three) {
-            inputCollection.push("3")
-        }
-        else if (item == four) {
-            inputCollection.push("4")
-        }
-        else if (item == five) {
-            inputCollection.push("5")
-        }
-        else if (item == six) {
-            inputCollection.push("6")
-        }
-        else if (item == seven) {
-            inputCollection.push("7")
-        }
-        else if (item == eight) {
-            inputCollection.push("8")
-        }
-        else if (item == nine) {
-            inputCollection.push("9")
-        }
-        else if (item == zero) {
-            inputCollection.push("0")
-        }
-
-        inputString = inputCollection.join("")
-
-
-        // console.log(inputString)
-        // console.log(inputCollection)
-
-        display.textContent = inputString
-    })
+    item.addEventListener('click', getNumber)
 })
+
+function getNumber() {
+
+    if (this == one){
+        inputCollection.push("1")
+    }
+    else if (this == two) {
+        inputCollection.push("2")
+    }
+    else if (this == three) {
+        inputCollection.push("3")
+    }
+    else if (this == four) {
+        inputCollection.push("4")
+    }
+    else if (this == five) {
+        inputCollection.push("5")
+    }
+    else if (this == six) {
+        inputCollection.push("6")
+    }
+    else if (this == seven) {
+        inputCollection.push("7")
+    }
+    else if (this == eight) {
+        inputCollection.push("8")
+    }
+    else if (this == nine) {
+        inputCollection.push("9")
+    }
+    else if (this == zero) {
+        inputCollection.push("0")
+    }
+    else if (this == dot) {
+        inputCollection.push(".")
+    }
+
+    inputNumber = inputCollection.join("")
+    console.log(inputNumber)
+    display.textContent = inputNumber
+}
+
+
+operators.forEach(item => {item.addEventListener('click', operate)
+})
+
+function operate() {
+    
+    operant = inputNumber
+    inputCollection = []
+    console.log(operant)
+
+    if(this == plus) {
+        equal.addEventListener('click', event => {
+            computed = parseInt(operant) + parseInt(inputNumber)
+            console.log(computed)
+            display.textContent = computed
+        })
+    }
+    else if(this == minus) {
+        equal.addEventListener('click', event => {
+            computed = parseInt(operant) - parseInt(inputNumber)
+            console.log(computed)
+            display.textContent = computed
+        })
+    }
+
+}
+
 
 
